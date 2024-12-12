66e397d140196c4d6dcefc5b02061e5fbbcc55d1
doaaabulatifa <155691860+doaaabulatifa@users.noreply.github.com>
2024-12-12 12:16:07 +0000

    Create imple-interest.sh

---
+++ imple-interest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter Principal Amount:"
+read principal
+echo "Enter Rate of Interest:"
+read rate
+echo "Enter Time (in years):"
+read time
+
+interest=$(echo "$principal * $rate * $time / 100" | bc)
+echo "The Simple Interest is: $interest"
